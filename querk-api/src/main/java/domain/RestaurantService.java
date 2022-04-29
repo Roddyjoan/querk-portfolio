300179a495ce2721ef19465bf63c8eb5c56d36bd
Erwyn Perez <e.perez@Erwyns-MacBook-Pro.local>
2022-04-29 16:04:59 -0400

    worked on the service layer and validations

--- querk-api/src/main/java/domain/RestaurantService.java
+++ querk-api/src/main/java/domain/RestaurantService.java
@@ -15,11 +15,15 @@ public class RestaurantService {
         this.repository = repository;
     }
 
-    public List<Restaurant> findAll(){ return repository.findAll(); }
+    public List<Restaurant> findAll() {
+        return repository.findAll();
+    }
 
-    public Restaurant findById(int restaurantId){ return repository.findById(restaurantId); }
+    public Restaurant findById(int restaurantId) {
+        return repository.findById(restaurantId);
+    }
 
-    public Result<Restaurant> add(Restaurant restaurant){
+    public Result<Restaurant> add(Restaurant restaurant) {
         Result<Restaurant> result = validate(restaurant);
         if (!result.isSuccess()) {
             return result;
@@ -35,14 +39,43 @@ public class RestaurantService {
         return result;
     }
 
-    private Result<Restaurant> validate(Restaurant restaurant){
-
-        Result<Restaurant> result = new Result<>();
-        if (restaurant == null) {
-            result.addMessage("customer cannot be null", ResultType.INVALID);
+    public Result<Restaurant> update(Restaurant restaurant) {
+        Result<Restaurant> result = validate(restaurant);
+        if (!result.isSuccess()) {
             return result;
         }
+
+        if (restaurant.getRestaurantId() <= 0) {
+            result.addMessage("RestaurantId must be set for `update` operation", ResultType.INVALID);
+            return result;
+        }
+
+        if (!repository.update(restaurant)) {
+            String msg = String.format("RestaurantId: %s, not found", restaurant.getRestaurantId());
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+
         return result;
     }
 
+    private Result<Restaurant> validate(Restaurant restaurant) {
+
+        Result<Restaurant> result = new Result<>();
+        if (restaurant == null) {
+            result.addMessage("restaurant cannot be null", ResultType.INVALID);
+            return result;
+        }
+
+        if (restaurant.getName() == null) {
+            result.addMessage("restaurant must have a name", ResultType.INVALID);
+            return result;
+        }
+
+        if (restaurant.getAddress() == null) {
+            result.addMessage("restaurant must have a valid address", ResultType.INVALID);
+        }
+
+        return result;
+
+    }
 }
---
+++ querk-api/src/main/java/domain/Validations.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Validations {
+
+    public static boolean isNullOrBlank(String value) {
+        return value == null || value.isBlank() || value.isEmpty();
+    }
+}
