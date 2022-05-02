02dd908b310d9bffa8725fcb13cf5bee233b54eb
Erwyn Perez <e.perez@erwyns-mbp.myfiosgateway.com>
2022-05-02 16:22:20 -0400

    schemas updated to add customers and restaurants

--- querk-api/src/main/java/learn/controllers/CustomerController.java
+++ querk-api/src/main/java/learn/controllers/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
-@RequestMapping("/api/customer")
+@RequestMapping("/api/customers")
 public class CustomerController {
     private final CustomerService service;
 
--- schemas/querk-prod-schemas.sql
+++ schemas/querk-prod-schemas.sql
@@ -88,15 +88,36 @@ insert into app_user_role
     (1, 2),
     (2, 1),
     (3,3);
-
-
-
-
-insert into restaurants_customers (restaurant_id,customer_id,create_time,ordered_ahead,expired) 
-values (1,1,"0001-01-01 00:00:00.0000000", true, false),
-(1,1,"0001-01-02 00:00:00.0000000", true, false);
-
-select * from restaurants_customers order by queue_create_time asc;
+    
+insert into customers (`name`, phone_num, email) values 
+		("roddy", "1231231234", "roddy@gamil.com" ),
+        ("kayleen", "9876543210", "kayleen@gmail.com" ),
+        ("erwyn", "9173886944", "erwyn@gmail.com");
+        
+	insert into restaurants (`name`, address, est) values
+		("burrito restaurant", "300 burrito lane", "00:20:00.0000000"),
+        ("Ichiran", "132 W 31st St, New York, NY 10001", "01:00:00.0000000"),
+        ("Hyun", "10 E 33rd St, New York, NY 10016", "00:45:15.0000000");
+    
+    insert into restaurants_customers (customer_id, restaurant_id, create_time, ordered_ahead, expired) values
+		(1, 1, "2011-01-01 01:11:00.0000000", true, false),
+        (1, 2, "2012-02-02 02:12:00.0000000", false, true),
+        (1, 3, "2013-03-03 03:13:00.0000000", false, true),
+        (2, 1, "2014-04-04 04:14:00.0000000", true, false),
+        (2, 2, "2015-05-05 05:15:00.0000000", false, true),
+        (2, 3, "2016-06-06 06:16:00.0000000", false, true),
+        (3, 1, "2017-07-07 07:17:00.0000000", false, true),
+        (3, 2, "2018-08-08 08:18:00.0000000", false, true),
+        (3, 3, "2019-09-09 09:19:00.0000000", false, true);
+        
+	insert into items ( restaurant_id, `name`, category, price, `description`) values
+		(1, "Small burrito", "entree", 7.43, "Burrito"),
+        (1, "Super burrito", "entree", 10.68, "enhanced burrito w/Cheesy-Salsa"),
+        (1, "dessert burrito", "dessert", 10.00, "a burrito but dessert"),
+        (2, "Ramen Kae-Dama", "entree", 30.00, "Specialized Ramen Noodle Soup"),
+        (2, "Matcha Ice Cream", "dessert", 35.00, "Matcha(Green) IceCream"),
+        (3, "Wagyu", "entree", 7.43, "Expensive beef but Hey its good beef"),
+        (3, "Taiyaki", "dessert", 7.00, "A fried delicacy with Ice Cream");
 
 
 
