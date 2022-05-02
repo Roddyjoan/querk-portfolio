3f5884b600f2dd26f4c4c47a501599aeea143042
Kayleen Chin <kchin4@stevens.edu>
2022-05-02 14:23:32 -0400

    customer service test

---
+++ querk-api/src/test/java/learn/domain/RestaurantQueueServiceTest.java
@@ -0,0 +1,32 @@
+package learn.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantQueueServiceTest {
+
+    @Test
+    void findAllByRestaurantId() {
+    }
+
+    @Test
+    void findAllNonExpiredByRestaurantId() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void makeExpired() {
+    }
+
+    @Test
+    void deleteByEntryId() {
+    }
+}
