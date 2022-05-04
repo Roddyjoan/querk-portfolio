1fa0a981f215773d5765118c0b5d6fca7704c0e7
Roddy <rratsirarson@dev-10.com>
2022-05-04 19:54:26 -0400

    Revert "some parts before I pull"
    This reverts commit 7d1dcb2914759490016ce42e5d623fa26d0a6601.

--- client/src/Signup/AddUserForm.js
+++ client/src/Signup/AddUserForm.js
@@ -1,24 +1,7 @@
-import { useState, useNavigate,useContext } from 'react';
-import './signup.css'
 function AddUserForm(){
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-
-    function submitHandler(event) {
-        event.preventDefault()
-    }
 
     return (
-        <div className="login-page">
-            <form onSubmit={submitHandler}>
-            <label>Username:</label><br />
-            <input onChange={event => setUsername(event.target.value)}></input><br /><br />
-            <label>Password:</label><br />
-            <input type="password" onChange={event => setPassword(event.target.value)}></input><br /><br />
-            <button>Sign In</button>
-            </form>
-        </div>
+        <p> This is the signup Form for customers</p>
     )
 }
 
