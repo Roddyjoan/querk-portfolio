295304a9a3b639a82ee765a24db1eb4b063f2c68
Erwyn Perez <e.perez@erwyns-mbp.myfiosgateway.com>
2022-05-05 10:18:52 -0400

    app.js changes for customer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./Home";
 import Nav from "./Nav";
 import Login from "./Login";
 import Customer from "./Customer/Customer"
+import Customers from './Customer/Customers';
 import NotFound from './NotFound';
 import jwtDecode from 'jwt-decode';
 import RequestPage from './Signup/RequestPage';
@@ -17,6 +18,7 @@ import Items from './Item/Items';
 import About from './About';
 import Terms from './Terms';
 import PrivacyPolicy from './PrivacyPolicy';
+import Customers from './Customer/Customers';
 
 function App() {
 
@@ -81,6 +83,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="*" element={<NotFound/>} />
             <Route path="/customer" element={<Customer customerObj={customer} />} />
+            <Route path="/customers" element={<Customers />} />
             <Route path="/signup" element={<RequestPage />} />
             <Route path="signup/customer" element={<AddUserForm />} />
             <Route path="signup/owner" element={<AddRestaurantForm />} />
