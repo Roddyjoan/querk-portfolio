7c3fe26515e5d6ef4928d65415a77893d1bd8f02
Kayleen Chin <kchin4@stevens.edu>
2022-05-06 14:47:48 -0400

    idk

---
+++ client/src/Restaurant/Queue.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+function Queue(props) {
+    const {customerId, name, phoneNum, email} = props.customerObj;
+
+    return (
+        <div className="customer-card">
+            <p><b>Name:</b> {name}</p>
+            <p><b>Phone #:</b> {phoneNum}</p>
+            <p><b>Email:</b> {email}</p>
+            <Link to={'/deleteQueue/' + customerId}><button>Delete Customer</button></Link>
+            <Link to={'/editQueue/' + customerId}><button>Edit Customer Information</button></Link>
+        </div>
+    )
+
+}
+
+export default Queue;
