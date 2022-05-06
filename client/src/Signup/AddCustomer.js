7c39ad43e2c70112739162d751bba472fc28cb78
Roddy <rratsirarson@dev-10.com>
2022-05-06 14:01:02 -0700

    almost all signup stuff

--- client/src/Signup/AddCustomer.js
+++ client/src/Signup/AddCustomer.js
@@ -6,7 +6,8 @@ function AddCustomer() {
     const [name, setName] = useState("");
     const [phoneNum, setPhoneNum] = useState("");
     const [email, setEmail] = useState("");
-    
+    const [password, setPassword] = useState("");
+
     const navigate = useNavigate();
 
     function handleName(e) {
@@ -20,12 +21,47 @@ function AddCustomer() {
     function handleEmail(e) {
         setEmail(e.target.value);
     }
+    function handlePassword(e){
+        setPassword(e.target.value);
+    }
+    const appUser = {
 
-    function handleSubmit(e){
-        e.preventDefault();
+    };
+    function postUser() {
+        const newUser = {
+            username: email,
+            password: password,
+            roles: ["CUSTOMER"]
+        };
 
-        const newCustomer= {
+        fetch("http://localhost:8090/api/user/customer", {
+
+            
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newUser)
+        }).then(response => {
+            if (response.ok) {
+                alert("Successfully added user");
+            } else {
+                alert("Could not add user. Make sure that the password has at least one special letter and one number.");
+                console.log(response);
+            }
+        }).then(data =>{
+            const dataObj = data.json;
+            console.log(dataObj);
+        })
+        .catch(
+            rejection => console.log("Failure! ", rejection)
+        );
+    }
+
+    function postCustomer() {
+        const newCustomer = {
             name: name,
+            userId: appUser.appUserId,
             phone: phoneNum,
             email: email
         };
@@ -44,32 +80,38 @@ function AddCustomer() {
             } else {
                 alert("Could not add customer.");
                 console.log(response);
-            } 
+            }
         }).catch(
             rejection => console.log("Failure! ", rejection)
         );
     }
 
-    return(
+    function handleSubmit(e) {
+        e.preventDefault();
+        postUser();
+
+    }
+
+    return (
         <>
             <div>
-            <h2>Customer Sign Up</h2><br />
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name:</label><br />
-                <input onChange={handleName} id="name"></input><br /><br />
+                <h2>Customer Sign Up</h2><br />
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="name">Name:</label><br />
+                    <input onChange={handleName} id="name"></input><br /><br />
 
-                <label htmlFor="phone#">Phone Number:</label><br />
-                <input onChange={handlePhone} id="phone#"></input><br /><br />
+                    <label htmlFor="phone#">Phone Number:</label><br />
+                    <input onChange={handlePhone} id="phone#"></input><br /><br />
 
-                <label htmlFor="email">Email:</label><br />
-                <input onChange={handleEmail} id="email"></input><br /><br />
+                    <label htmlFor="email">Email:</label><br />
+                    <input onChange={handleEmail} id="email"></input><br /><br />
 
-                <label htmlFor="email">Password:</label><br />
-                <input onChange={handleEmail} id="email" type="password"></input><br /><br />
+                    <label htmlFor="email">Password:</label><br />
+                    <input onChange={handlePassword} id="email" type="password"></input><br /><br />
 
-                <button type="submit">Sign Up</button>
-                <Link to={'/Home'}><button>Cancel</button></Link>
-            </form>
+                    <button type="submit">Submit</button>
+                    <Link to={'/Home'}><button>Cancel</button></Link>
+                </form>
             </div>
         </>
     )
