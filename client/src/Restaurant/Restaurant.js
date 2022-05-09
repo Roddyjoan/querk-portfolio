75db14228f8d7696d6ace8a66a4fb50220a4c23f
Roddy <rratsirarson@dev-10.com>
2022-05-09 07:20:30 -0700

    newwest commit

--- client/src/Restaurant/Restaurant.js
+++ client/src/Restaurant/Restaurant.js
@@ -1,16 +1,22 @@
 import { Link } from 'react-router-dom';
 
 function Restaurant(props) {
-    const {restaurantId, name, address, est} = props.restaurantObj;
+    const { restaurantId, name, address, est } = props.restaurantObj;
 
     return (
-        <div className="restaurant-card">
+        <><div className="restaurant-card">
+            <div className="map">
+                <p> this shuld be the map</p>
+            </div>
             <p><b>Name:</b> {name}</p>
             <p><b>Address:</b> {address}.</p>
             <p><b>Estimated Wait Time:</b> {est}</p>
             <Link to={'/menu/' + restaurantId}><button>Menu</button></Link>
             <Link to={'/restaurant/queue/current/' + restaurantId}><button>Join Queue</button></Link>
+
         </div>
+
+        </>
     )
 
 }
